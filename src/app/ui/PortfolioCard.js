d0fef2f8e41430f259ad6fed0bc2a9c5ff0560a4
lyovson <rafa@lyovson.com>
2023-12-14 03:04:12 +0400

    Update PortfolioCard layout and header height

--- src/app/ui/PortfolioCard.js
+++ src/app/ui/PortfolioCard.js
@@ -5,11 +5,11 @@ const PortfolioCard = ({ post }) => {
   return (
     <motion.article
       layout
-      className="flex flex-col justify-between p-4 border aspect-square"
+      className="flex flex-col justify-between p-4 border aspect-square grow"
       transition={{ duration: 0.5 }}
     >
-      <header className="flex flex-col justify-between gap-4 h-[120px] ">
-        <h2 className="text-2xl capitalize">{post.title}</h2>
+      <header className="flex flex-col justify-between gap-4 h-[200px] ">
+        <h2 className="text-2xl capitalize ">{post.title}</h2>
         <div className="h-2 w-[100%] mx-auto rounded-lg bg-gradient-to-r from-jess to-rafa"></div>
       </header>
 
