99682c1d14f4720399189933458045f90c6af80a
lyovson <rafa@lyovson.com>
2023-12-16 04:00:52 +0400

    Refactor PortfolioCard component styling

--- src/app/ui/PortfolioCard.js
+++ src/app/ui/PortfolioCard.js
@@ -1,20 +1,23 @@
 "use client";
 
 import { motion } from "framer-motion";
+
 const PortfolioCard = ({ post }) => {
   return (
     <motion.article
       layout
-      className="flex flex-col justify-between p-4 border aspect-square grow"
+      className="relative flex flex-col justify-between overflow-hidden bg-white border rounded-lg shadow-lg aspect-square grow dark:bg-gray-800"
       transition={{ duration: 0.5 }}
     >
-      <header className="flex flex-col justify-between gap-4 h-[200px] ">
-        <h2 className="text-2xl capitalize ">{post.title}</h2>
-        {/* <div className="h-2 w-[100%] mx-auto rounded-lg bg-gradient-to-r from-jess to-rafa"></div> */}
-        <img src={post.featuredImg} alt="test" />
+      <img
+        className="absolute object-cover w-full h-full"
+        src={post.featuredImg}
+        alt="test"
+      />
+      <div className="absolute inset-0 "></div>
+      <header className="absolute bottom-0 w-full p-4 dark:bg-dark/50 bg-light/50">
+        <h2 className="text-2xl capitalize">{post.title}</h2>
       </header>
-
-      <div>{post.content}</div>
     </motion.article>
   );
 };
