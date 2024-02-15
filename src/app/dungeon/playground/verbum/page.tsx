b81f16d90e6a5b5bc7768cd35f9d7b0874d46e59
lyovson <rafa@lyovson.com>
2024-02-15 21:41:37 +0400

    Refactor NoteViewer component to page component

--- src/app/dungeon/playground/verbum/page.tsx
+++ src/app/dungeon/playground/verbum/page.tsx
@@ -1,48 +1,7 @@
-"use client";
-import { FC } from "react";
-import {
-  AlignDropdown,
-  BackgroundColorPicker,
-  BoldButton,
-  CodeFormatButton,
-  Divider,
-  Editor,
-  EditorComposer,
-  FontFamilyDropdown,
-  FontSizeDropdown,
-  InsertDropdown,
-  InsertLinkButton,
-  ItalicButton,
-  TextColorPicker,
-  TextFormatDropdown,
-  ToolbarPlugin,
-  UnderlineButton,
-} from "verbum";
+//
 
-const NoteViewer: FC = () => {
-  return (
-    <EditorComposer>
-      <Editor hashtagsEnabled={true}>
-        <ToolbarPlugin defaultFontSize="20px">
-          <FontFamilyDropdown />
-          <FontSizeDropdown />
-          <Divider />
-          <BoldButton />
-          <ItalicButton />
-          <UnderlineButton />
-          <CodeFormatButton />
-          <InsertLinkButton />
-          <TextColorPicker />
-          <BackgroundColorPicker />
-          <TextFormatDropdown />
-          <Divider />
-          <InsertDropdown enablePoll={true} />
-          <Divider />
-          <AlignDropdown />
-        </ToolbarPlugin>
-      </Editor>
-    </EditorComposer>
-  );
+const page = () => {
+  return <div>page</div>;
 };
 
-export default NoteViewer;
+export default page;
