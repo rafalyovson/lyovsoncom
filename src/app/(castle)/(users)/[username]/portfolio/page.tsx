8f94201d8a4880b93f076df1d4c3f6e55970eda5
lyovson <rafa@lyovson.com>
2024-07-23 17:52:21 +0400

    removed test pages

--- src/app/(castle)/(users)/[username]/portfolio/page.tsx
+++ src/app/(castle)/(users)/[username]/portfolio/page.tsx
@@ -1,19 +1,19 @@
-import { userSelectFullOneByUsername } from '@/lib/actions/db-actions/user/user-select-full-one';
-import { redirect } from 'next/navigation';
+async function Page({ params }: { params: { username: string } }) {
+  console.log('params', params);
+  // const username = params.username;
+  // const result = await userSelectFullOneByUsername({
+  //   username,
+  // });
 
-export async function Page({ params }: { params: { username: string } }) {
-  const username = params.username;
-  const result = await userSelectFullOneByUsername({
-    username,
-  });
-
-  if (!result.success || !result.user) {
-    redirect('/');
-  }
+  // if (!result.success || !result.user) {
+  //   redirect('/');
+  //
+  // }
   return (
-    <h1
-      className={`text-2xl text-center`}
-    >{`${result.user.name}'s Portfolio`}</h1>
+    <h1 className={`text-2xl text-center`}>
+      {/*{`${result.user.name}'s Portfolio`}*/}
+      Portfolio
+    </h1>
   );
 }
 
