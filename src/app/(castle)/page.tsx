af0fc3326ea3eb5d30cafc3bc1c6239e2bdcf25b
lyovson <rafa@lyovson.com>
2024-10-09 23:17:27 +0400

    fixed some long-annoying issues oin the front

--- src/app/(castle)/page.tsx
+++ src/app/(castle)/page.tsx
@@ -8,16 +8,14 @@ export default async function HomePage() {
     return <div>{result.message}</div>;
   }
 
+  const posts = result.posts.filter((post) => post.published);
+
   return (
     <>
       <main className="flex flex-col items-center gap-12 p-6 sm:p-8 md:p-12 lg:p-16 bg-background text-gray-800 dark:text-gray-200">
         <About />
-        <section className="w-full max-w-screen-xl px-4">
-          <h2 className="text-3xl lg:text-4xl font-bold text-primary dark:text-primary-light mb-8 text-center lg:text-left">
-            Latest Posts
-          </h2>
-          <PostGrid posts={result.posts} />
-        </section>
+
+        <PostGrid posts={posts} />
       </main>
     </>
   );
--- src/components/castle/footer/footer-menu.tsx
+++ src/components/castle/footer/footer-menu.tsx
@@ -4,10 +4,7 @@ import ThemeSwitch from '@/components/theme-switcher';
 import { Button } from '@/components/shadcn/ui/button';
 import {
   faArrowRightFromBracket,
-  faDungeon,
   faFileText,
-  faGamepad,
-  faPlus,
   faRightToBracket,
   faTowerObservation,
 } from '@fortawesome/free-solid-svg-icons';
@@ -19,59 +16,13 @@ export const FooterMenu = () => {
   const { data: session } = useSession();
 
   return (
-    <nav className="flex justify-center items-center p-6 bg-background text-gray-800 dark:text-gray-200 border-t border-gray-300 dark:border-gray-700">
+    <nav className="flex justify-center items-center p-6 bg-background text-gray-800 dark:text-gray-200 ">
       {session ? (
         <section className="flex flex-row items-center gap-6 text-center flex-wrap">
           <Button
             asChild
             size="icon"
             variant={'secondary'}
-            className="hover:bg-primary/80"
-            aria-label="Create a new post"
-          >
-            <Link href="/dungeon/posts/create">
-              <FontAwesomeIcon
-                icon={faPlus}
-                className="text-primary dark:text-primary-light"
-              />
-            </Link>
-          </Button>
-
-          <Button
-            asChild
-            size="icon"
-            variant={'secondary'}
-            className="hover:bg-primary/80"
-            aria-label="Return to the entrance of the dungeon"
-          >
-            <Link href="/dungeon">
-              <FontAwesomeIcon
-                icon={faDungeon}
-                className="text-primary dark:text-primary-light"
-              />
-            </Link>
-          </Button>
-
-          <Button
-            asChild
-            size="icon"
-            variant={'secondary'}
-            className="hover:bg-primary/80"
-            aria-label="Visit the playground"
-          >
-            <Link href={{ pathname: '/dungeon/playground' }}>
-              <FontAwesomeIcon
-                icon={faGamepad}
-                className="text-primary dark:text-primary-light"
-              />
-            </Link>
-          </Button>
-
-          <Button
-            asChild
-            size="icon"
-            variant={'secondary'}
-            className="hover:bg-primary/80"
             aria-label="Return to the tower"
           >
             <Link href="/public">
@@ -86,7 +37,6 @@ export const FooterMenu = () => {
             asChild
             size="icon"
             variant={'secondary'}
-            className="hover:bg-primary/80"
             aria-label="Read all posts"
           >
             <Link href="/posts">
@@ -117,7 +67,6 @@ export const FooterMenu = () => {
           <Button
             size="icon"
             variant={'secondary'}
-            className="hover:bg-primary/80"
             onClick={() => signIn()}
             aria-label="Sign in"
           >
